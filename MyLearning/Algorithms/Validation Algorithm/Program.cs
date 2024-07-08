68f3a140be622bb696f23e2643d8c60c0c07250b
Minister124 <kushaltmg321@gmail.com>
2024-07-08 14:15:48 +0545

    Upper case Validation Algo

---
+++ MyLearning/Algorithms/Validation Algorithm/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+
+namespace  Validation_Algorithm
+{
+    class Program
+    {
+        static bool IsUppercase(string str)
+        {
+            return str.All(char.IsUpper);
+        }
+        static void Main(string[] args)
+        {
+            WriteLine(IsUppercase("HELLO"));
+        }
+    }
+}
