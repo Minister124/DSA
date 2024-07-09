589e1f4f43e2cb823b842d5c4eab06949c5ca67c
Minister124 <kushaltmg321@gmail.com>
2024-07-09 13:15:27 +0545

    To Lower

--- MyLearning/String Algortihms/Normalize Strings/Program.cs
+++ MyLearning/String Algortihms/Normalize Strings/Program.cs
@@ -1,9 +1,19 @@
-﻿namespace Normalize_Strings;
+﻿using System.Diagnostics;
+
+namespace Normalize_Strings;
 
 class Program
 {
+
+    static string NormalizeString(string input){
+        string lowerInput = input.ToLower();
+        return lowerInput;
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Stopwatch sw = Stopwatch.StartNew();
+        WriteLine(NormalizeString("Hello"));
+        sw.Stop();
+        WriteLine($"Execution Time: {sw.ElapsedMilliseconds} ms");
     }
 }
