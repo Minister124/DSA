7e12d6e34a6a53f34476ad9fd687e66e0d565ecd
Minister124 <kushaltmg321@gmail.com>
2024-07-11 09:15:07 +0545

    Parse String

--- MyLearning/String Algortihms/Parse and Search Strings/Program.cs
+++ MyLearning/String Algortihms/Parse and Search Strings/Program.cs
@@ -1,2 +1,30 @@
-﻿
-Console.WriteLine("Hello, World!".ToLower().Contains("ll"));
+﻿using System;
+
+namespace Parse_and_Search_Strings
+{
+    class Program
+    {
+        static void ParseString(string s){
+            Console.WriteLine("Option 1");
+
+            foreach (Char item in s)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Option 2");
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                char c = s[i];
+                Console.WriteLine(c);    
+            }    
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!".ToLower().Contains("ll"));
+        }
+    }
+}
+
