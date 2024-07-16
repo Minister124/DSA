87546e2ec7cc620aa8042f4eae184bf881f343ae
Minister124 <kushaltmg321@gmail.com>
2024-07-16 20:44:56 +0545

    Methods

--- MyLearning/Array Algortihms/Linear Search/Program.cs
+++ MyLearning/Array Algortihms/Linear Search/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+
+namespace LinearSearch;
+
+class Program{
+
+    static bool LinearSearchArray(int[] ary, int n){
+        foreach (int item in ary)
+        {
+            if(item == n) return true;
+        }
+        return false;
+    }
+    static void Main(string[] args){
+        int[] arry = {1, 2, 5, 9, 7, 8};
+        int a = 2;
+
+        Console.WriteLine(LinearSearchArray(arry, a));
+    }
+}
