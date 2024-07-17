4877509c3b92fa15d70cd21489cf89b0b011ddb1
Minister124 <kushaltmg321@gmail.com>
2024-07-17 10:55:21 +0545

    Built in Methods of Array

--- MyLearning/Array Algortihms/Linear Search/Program.cs
+++ MyLearning/Array Algortihms/Linear Search/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using static System.Console;
 
 namespace LinearSearch;
@@ -18,12 +19,33 @@ class Program{
 
         //Console.WriteLine(LinearSearchArray(arry, a));
 
-        int item = Array.Find(arry, item => item == 5);
-        int sheesh = Array.Find(arry, item => item == 3);
-        Console.WriteLine(item);
-        WriteLine(sheesh);
+        int item = Array.Find(arry, item => item == 5); // Find the element 5 in the array
+        int sheesh = Array.Find(arry, item => item == 3); // Find the element 3 in the array
+        // WriteLine(item);
+        // WriteLine(sheesh);
 
-        int[] items = Array.FindAll(arry, items => items >= 5);
-        Array.ForEach(items, WriteLine);
+        // int[] items = Array.FindAll(arry, items => items >= 5); // Find all elements greater than or equal to 5
+        // Array.ForEach(items, WriteLine);
+
+        // Array.Sort(arry); // Sorts the array in ascending order
+        // Array.ForEach(arry, WriteLine);
+
+        //  Array.Reverse(arry); // Reverses the array
+        //  Array.ForEach(arry, WriteLine);
+
+        // int indexOf5 = Array.IndexOf(arry, 5);
+        // int indexOf2 = Array.IndexOf(arry, 2);
+        // WriteLine(indexOf5); // 2
+        // WriteLine(indexOf2); // 1
+
+        // int[] copied = new int[arry.Length];
+        // Array.Copy(arry, copied, arry.Length); Copies the array to another array with same length
+        // Array.ForEach(copied, WriteLine);
+
+        // int lastIndex = Array.LastIndexOf(arry, 1);
+        // WriteLine(lastIndex); // 0
+
+        // Array.Fill(arry, 0);
+        // Array.ForEach(arry, WriteLine); // Fills value 0 to all elements in the array
     }  
 }
