61ba2d8cf3b2ae129f1509dd3bedec065141f298
Minister124 <kushaltmg321@gmail.com>
2024-07-23 23:21:06 +0545

    Complete Binary Search

--- MyLearning/Array Algortihms/Binary Search/Program.cs
+++ MyLearning/Array Algortihms/Binary Search/Program.cs
@@ -2,8 +2,29 @@
 
 class Program
 {
+
+    static bool BinarySearch(int[] input, int item){
+        int min = 0;
+        int max = input.Length - 1;
+        
+        while(min <= max){
+            int mid = (min + max) / 2;
+            if (item == input[mid]){
+                return true;
+            } else if (item < input[mid]){
+                max = mid - 1;
+            } else{
+                min = mid + 1;
+            }
+        }
+        return false;
+    }
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int[] arr = {1, 10, 30, 40, 80};
+
+        Console.WriteLine(BinarySearch(arr, 10));
+        Console.WriteLine(Array.BinarySearch(arr, 40));
     }
 }
