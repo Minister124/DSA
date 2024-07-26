984b666ec7ab1ede38b555c0f50d16012fe1c05b
Minister124 <kushaltmg321@gmail.com>
2024-07-26 16:11:23 +0545

    In Place Reverse

--- MyLearning/Array Algortihms/ReverseArray/Program.cs
+++ MyLearning/Array Algortihms/ReverseArray/Program.cs
@@ -9,11 +9,27 @@
             }
             return reversed;
         }
+
+        static void ReverseInPlace(int[] arr){
+            int start = 0;
+            int end = arr.Length - 1;
+            while(start < end){
+                int temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+                start++;
+                end--;
+            }
+        }
         static void Main(string[] args)
         {
             int[] ar = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
             int[] reversed = ReverseArray(ar);
-            Array.ForEach(reversed, num => Console.WriteLine(num));
+            // Array.ForEach(reversed, num => Console.WriteLine(num));
+            // Array.ForEach(reversed, num => Console.WriteLine(num));
+            ReverseInPlace(ar);
+            ReverseInPlace(ar);
+            Array.ForEach(ar, Console.WriteLine);
         }
     }
 }
