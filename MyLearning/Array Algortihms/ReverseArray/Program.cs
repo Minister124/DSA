8ed5927578cdfd71661cd5c553c0931d588c2d89
Minister124 <kushaltmg321@gmail.com>
2024-07-26 16:02:05 +0545

    Reversed Array

--- MyLearning/Array Algortihms/ReverseArray/Program.cs
+++ MyLearning/Array Algortihms/ReverseArray/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace ReverseArray
+{
+    class Program{
+        
+        static int[] ReverseArray(int[] arr){
+            int[]  reversed = new int[arr.Length];
+            for(int i = arr.Length -1; i >=0; i--){
+                reversed[arr.Length - 1 - i] = arr[i];
+            }
+            return reversed;
+        }
+        static void Main(string[] args)
+        {
+            int[] ar = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int[] reversed = ReverseArray(ar);
+            Array.ForEach(reversed, num => Console.WriteLine(num));
+        }
+    }
+}
