d34e6ceb754738fdcacac9adfbff78be85fde514
Minister124 <kushaltmg321@gmail.com>
2024-07-26 14:35:21 +0545

    Completed Aggregate and Filtering Arrays

--- MyLearning/Array Algortihms/AggregateAndFilterArrays/Program.cs
+++ MyLearning/Array Algortihms/AggregateAndFilterArrays/Program.cs
@@ -1,9 +1,35 @@
-﻿namespace AggregateAndFilterArrays;
+﻿using System.Collections;
+
+namespace AggregateAndFilterArrays;
 
 class Program
 {
+    static int[] FindEvenNums(int[] nums, int[] nums2)
+    {
+        ArrayList list = new ArrayList();
+        
+        foreach (int num in nums)
+        {
+            if (num % 2 == 0)
+            {
+                list.Add(num);
+            }
+        }
+        foreach (int num in nums2)
+        {
+            if (num % 2 == 0)
+            {
+                list.Add(num);
+            }
+        }
+        return (int[])list.ToArray(typeof(int));
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int[] ar1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        int[] ar2 = {0, -2, -9, -39, 39, 10};
+
+        int[] evenNums = FindEvenNums(ar1, ar2);
+        Array.ForEach(evenNums, num => Console.WriteLine(num));
     }
 }
